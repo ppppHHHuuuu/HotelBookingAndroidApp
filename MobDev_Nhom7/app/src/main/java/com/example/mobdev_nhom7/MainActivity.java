b0904f57d119eec45c54275d9fdff018ce5a2318
unknown <vunhatminh317@gmail.com>
2023-10-04 11:18:36 +0700

    Initial commit

---
+++ MobDev_Nhom7/app/src/main/java/com/example/mobdev_nhom7/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobdev_nhom7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
