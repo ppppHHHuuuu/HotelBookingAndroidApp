c3155a6de0e6dc2f21090d8589468ea21b68b263
Nguyen Binh Nguyen <nguyenrtm@gmail.com>
2023-10-13 00:23:13 +0700

    hotel view activity

---
+++ app/src/main/java/com/example/mobdev_nhom7/ViewHotel.java
@@ -0,0 +1,17 @@
+package com.example.mobdev_nhom7;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.Button;
+
+import androidx.core.text.HtmlCompat;
+
+public class ViewHotel extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_hotel);
+
+    }
+}
