e3dbbddbc3d64ddec5d808162c948e36251ef53c
Phu <87368494+ppppHHHuuuu@users.noreply.github.com>
2023-10-15 10:22:20 +0700

    Add Favorite Activity

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MobDev_Nhom7"
+include(":app")
