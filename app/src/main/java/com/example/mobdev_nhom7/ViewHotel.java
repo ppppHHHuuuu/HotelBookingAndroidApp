821e410f6aa02db880e5bbb8aded8d25eac31b8b
Nguyen Binh Nguyen <nguyenrtm@gmail.com>
2023-10-16 17:23:08 +0700

    make price button changeable

--- app/src/main/java/com/example/mobdev_nhom7/ViewHotel.java
+++ app/src/main/java/com/example/mobdev_nhom7/ViewHotel.java
@@ -3,7 +3,9 @@ package com.example.mobdev_nhom7;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.core.text.HtmlCompat;
 
@@ -12,6 +14,8 @@ public class ViewHotel extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_hotel);
-
+        View price_button1 = findViewById(R.id.button1);
+        TextView room1 = (TextView) price_button1.findViewById(R.id.room);
+        room1.setText("Doubles Room");
     }
 }
