d978cbba95555f9842cfe78c4002bae761000e5a
Phu <87368494+ppppHHHuuuu@users.noreply.github.com>
2023-10-16 11:21:36 +0700

    Add Fragment Favourite

--- app/src/main/java/com/example/mobdev_nhom7/CardHotelAdapter.java
+++ app/src/main/java/com/example/mobdev_nhom7/CardHotelAdapter.java
@@ -1,7 +1,6 @@
-package com.build.mobdev_nhom7;
+package com.example.mobdev_nhom7;
 
 import android.content.Context;
-import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +9,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mobdev_nhom7.R;
 
 import java.util.ArrayList;
 
--- app/src/main/java/com/example/mobdev_nhom7/FavouritesFragment.java
+++ app/src/main/java/com/example/mobdev_nhom7/FavouritesFragment.java
@@ -2,46 +2,43 @@ package com.example.mobdev_nhom7;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FavouritesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class FavouritesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ArrayList<String> hotels;
+    ArrayList <String> stars;
+    ArrayList <String> cities;
+    ArrayList <String> scores;
+    ArrayList <String> rates;
+    ArrayList <String> judges;
+    ArrayList <Integer> images;
 
-    // TODO: Rename and change types of parameters
+    RecyclerView recyclerView;
+
+    CardHotelAdapter cardHotelAdapter;
+    private static final String TAG = "Favourite Fragment";
     private String mParam1;
-    private String mParam2;
 
     public FavouritesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FavouritesFragment.
-     */
     // TODO: Rename and change types and number of parameters
-    public static FavouritesFragment newInstance(String param1, String param2) {
+    public static FavouritesFragment newInstance(String param1) {
         FavouritesFragment fragment = new FavouritesFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(TAG, param1);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,15 +47,89 @@ public class FavouritesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam1 = getArguments().getString(TAG);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.fragment_favourites, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourites, container, false);
+
+        // Assign variables to views using findViewById
+//        textHotelName = findViewById(R.id.textHotelName);
+//        textStarNumber = findViewById(R.id.textStarNumber);
+//        textCity = findViewById(R.id.textCity);
+//        textScore = findViewById(R.id.textScore);
+//        textRate = findViewById(R.id.textRate);
+//        textNumberofJudges = findViewById(R.id.textNumberofJudges);
+        hotels = new ArrayList<String>(){{
+            add("Hotel 1");
+            add("Hotel 2");
+            add("Hotel 3");
+            add("Hotel 4");
+            add("Hotel Long Name does it lost");
+            add("Hotel 6");
+        }};
+        stars = new ArrayList<String>(){{
+            add("****");
+            add("*****");
+            add("***");
+            add("***");
+            add("***");
+            add("***");
+        }};
+        cities = new ArrayList<String>() {
+            {
+                add("Ha Noi");
+                add("Ho Chi Minh");
+                add("Da Nang");
+                add("Da Nang");
+                add("Da Nang");
+                add("Da Nang");
+            }
+        };
+        scores = new ArrayList<String>() {{
+            add("8.6");
+            add("4.5");
+            add("8.9");
+            add("8.9");
+            add("8.9");
+            add("8.9");
+        }};
+        rates = new ArrayList<String>() {{
+            add("Xuat sac");
+            add("Xuat sac");
+            add("Xuat sac");
+            add("Xuat sac");
+            add("Xuat sac");
+            add("Xuat sac");
+        }};
+        judges = new ArrayList<String>() {{
+            add("4546 nhan xet");
+            add("3456 nhan xet");
+            add("3546 nhan xet");
+            add("3546 nhan xet");
+            add("3546 nhan xet");
+            add("3546 nhan xet");
+        }};
+        images = new ArrayList<Integer>() {{
+            add(R.drawable.hotel_1);
+            add(R.drawable.hotel_2);
+            add(R.drawable.hotel_3);
+            add(R.drawable.hotel_3);
+            add(R.drawable.hotel_3);
+            add(R.drawable.hotel_3);
+        }};
+
+        cardHotelAdapter = new CardHotelAdapter(getActivity(), hotels, stars, cities, scores, rates, judges, images );
+        recyclerView = (RecyclerView) v.findViewById(R.id.recycleView);
+
+        recyclerView.setAdapter(cardHotelAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return v;
     }
+
+
 }
